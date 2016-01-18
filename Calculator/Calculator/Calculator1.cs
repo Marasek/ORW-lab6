5d000982caec645bde18f90305d0c79a74719006
Marek Tryner <marektryner@wp.pl>
2016-01-18 12:17:04 +0100

    First commit

---
+++ Calculator/Calculator/Calculator1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    public class Calculator1
+    {
+        Operator[] _operatory = new Operator[]
+        {
+            new Operator('+', (left,right) => { return left + right;} ),
+            new Operator('-', (left,right) => { return left - right;} ),
+            new Operator('*', (left,right) => { return left * right;} ),
+            new Operator('/', (left,right) => { return left / right;} )
+        };
+
+        class Operator
+        {
+            public char Symbol;
+            public Func<int, int, int> Function;
+            public Operator(char symbol, Func<int, int, int> function)
+            {
+                Symbol = symbol;
+                Function = function;
+            }
+        }
+
+        public int Calculate(string input)
+        {
+            foreach (var o in _operatory)
+            {
+                if (input.Contains(o.Symbol))
+                {
+                    string[] parts = input.Split(new char[] { o.Symbol }, StringSplitOptions.RemoveEmptyEntries);
+                    int left = Convert.ToInt32(parts[0]);
+                    int right = Convert.ToInt32(parts[1]);
+                    return o.Function(left, right);
+                }
+            }
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ Calculator/CalculatorTest/CalculatorTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Calculator1 calc = new Calculator1(); 
+        }
+
+        [TestMethod]
+        public void TestInput()
+        {
+            Calculator1 calc = new Calculator1();
+            try 
+            {
+                int result = calc.Calculate("abcd");
+                Assert.Fail();
+            }
+            catch (InvalidOperationException)
+            {
+
+            }
+        }
+
+        [TestMethod]
+        public void JedenplusJeden()
+        { 
+            Calculator1 calc = new Calculator1();
+            int result = calc.Calculate("1+1");
+            Assert.AreEqual(2, result);
+        }
+
+        void DoSumTest(string input, int expected)
+        {
+            Calculator1 calc = new Calculator1();
+            int result = calc.Calculate(input);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void DwaplusDwa()
+        {
+            DoSumTest("2+2", 4);
+        }
+        [TestMethod]
+        public void CzteryplusJedenascie()
+        {
+            DoSumTest("4+11", 15);
+        }
+        [TestMethod]
+        public void DwaminusJeden()
+        {
+            DoSumTest("2-1", 1);
+        }
+        [TestMethod]
+        public void PiecminusDwa()
+        {
+            DoSumTest("5-2", 3);
+        }
+        [TestMethod]
+        public void PiecrazyDwa()
+        {
+            DoSumTest("5*2", 10);
+        }
+        [TestMethod]
+        public void DziesiecpodzielicDwa()
+        {
+            DoSumTest("10/2", 5);
+        }
+    }
+}
